bee3fd8594e528cf1b85120edb9f0213aee97f1f
ritiksrivastava-dev <ritiksrivastava99@gmail.com>
2023-05-19 19:07:04 +0530

    Reversal of Linked List

---
+++ Linked Lists/Reversal_Linked_List_Iterative.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+using namespace std;
+
+//Basic Flow of the Program:
+/*
+    Once the linked list class is called from the main function, it by default constructs a head pointer with the value of NULL
+    This function only happens when we initially define a new list.
+*/
+
+/*
+    Later when inserting a new node, we instiantiate the class of node (in which we defined our data structure) which uses the 
+    parametrized constructor to define a new node into memory which is initially set to NULL, since we're adding node at the end
+    we need to change the link of the node before the last node.
+*/
+
+
+class Node {
+    public:
+        int data;
+        Node * link;
+
+    Node(int data){ //Parameterized constructor
+        this -> data = data;
+        this -> link = NULL;
+    }
+};
+
+
+class LinkedList{
+    Node* head;
+    
+    public:
+    LinkedList(){ head = NULL; } //default constructor
+
+    void insertNode(int);
+    void printList();
+    void reversal();
+};
+
+void LinkedList::insertNode(int data){
+    Node* newNode = new Node(data);
+    
+    if(head == NULL){
+        head = newNode;
+        return;
+    }
+
+    //if head != NULL --- using the while loop we go the end of the last node and change the address of the penultimate node
+    Node* temp = head;
+    while(temp->link != NULL){
+        temp = temp -> link;
+    }
+    temp->link = newNode;
+}
+
+void LinkedList::printList()
+{
+    Node* temp = head;
+  
+    // Check for empty list.
+    if (head == NULL) {
+        cout << "List empty" << endl;
+        return;
+    }
+  
+    // Traverse the list.
+    while(temp != NULL) {
+        cout << temp->data << " ";
+        temp = temp->link;
+    }
+}
+
+void LinkedList::reversal(){
+    Node *next = NULL;
+    Node *prev = NULL;
+    Node *curr = head;
+
+    while(curr != NULL){
+        next = curr->link;
+        curr -> link = prev;
+
+        prev = curr;
+        curr = next;
+    }
+    //Important - the head of the list starts from the previous node after reversal; at the end of the traversal of while loop,
+    //the curr and next pointers are pointing at null while the previous pointer is point at the last node of the linked list.
+    head = prev; 
+}
+
+int main(){
+    LinkedList list;
+
+    list.insertNode(1);
+    list.insertNode(2);
+    list.insertNode(3);
+
+    list.printList();
+
+    cout << endl;
+
+    list.reversal();
+    list.printList();
+    cout << endl;
+}
